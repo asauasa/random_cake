7052f2f5f2f524bd73abbca7f1d101d085f05c69
asaumi <asaumi@sorich.jp>
2017-03-13 20:13:18 +0900

    TableとLogic

---
+++ src/Model/Logic/Xxx.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Logic\Xxx;
+
+use Cake\ORM\TableRegistry;
+
+class Xxx
+{
+
+}
---
+++ src/Model/Table/AccessInfosTable.php
@@ -0,0 +1,19 @@
+<?php
+//strictモード(厳密な型チェック)
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\ORM\TableRegistry;
+use Cake\Validation\Validator;
+
+class AccessInfosTable extends Table
+{
+	public  function xxx()
+	{
+
+	}
+}
